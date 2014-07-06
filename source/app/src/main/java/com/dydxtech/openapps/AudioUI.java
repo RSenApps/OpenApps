81369162118be7ae3ff04f68924b21a9d15be630
Ryan Senanayake <rsenapps@gmail.com>
2014-07-06 00:59:42 -0400

    fix issue

--- source/app/src/main/java/com/dydxtech/openapps/AudioUI.java
+++ source/app/src/main/java/com/dydxtech/openapps/AudioUI.java
@@ -60,7 +60,7 @@ public class AudioUI {
         if (listenHotword) {
 
                 speechRecognizer = new GoogleSpeechRecognizer(context, this);
-
+                startListening();
         }
 
         TelephonyManager mgr = (TelephonyManager) context
